2df752144e0c7dae91c2b4383de27116781de7ca
same <broughtover@gmail.com>
2017-06-02 04:00:15 +0000

    new-column

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,7 @@
 class Message < ApplicationRecord
-    validates :titl , length: { maximum: 32 } , presence: true
+    validates :title , length: { maximum: 32 } , presence: true
     validates :message , length: { maximum: 256 } , presence: true
     validates :name , length: { maximum: 32 }
+    validates :weight , presence: true
 end
+ 
